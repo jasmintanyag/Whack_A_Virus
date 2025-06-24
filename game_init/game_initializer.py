8d41d0d30b8fbc95768cfd57321fe6fb474e784f
jasmintanyag <tanyagjasmin@gmail.com>
2025-06-24 16:00:34 +0800

    Import all the necessary libraries/modules.

---
+++ game_init/game_initializer.py
@@ -0,0 +1,9 @@
+import turtle
+import pygame
+import os
+from game_assets.image_handler import ImageHandler
+from game_assets.virus import Virus
+from game_assets.scoreboard import Scoreboard
+from game_assets.timer import Timer
+from game_assets.start_button import StartButton
+
