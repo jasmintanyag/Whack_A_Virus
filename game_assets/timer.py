8fd5301e86af61d604ae25fc1755d650b7f5cbfa
jasmintanyag <tanyagjasmin@gmail.com>
2025-06-24 15:47:38 +0800

    Initializing the game duration and setting the timer color to white.

---
+++ game_assets/timer.py
@@ -0,0 +1,11 @@
+from game_assets.base import GameElement
+
+class Timer(GameElement):
+    def __init__(self, screen, game_duration, callback):
+        super().__init__(screen)
+        self.time = game_duration
+        self.callback = callback
+        self.turtle.goto(0, 220)
+        self.turtle.color("white")
+
+    
