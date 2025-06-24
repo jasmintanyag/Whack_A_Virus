7f8e757f3e19e25d4180b6f7c639ecc2a7ff8337
jasmintanyag <tanyagjasmin@gmail.com>
2025-06-24 15:17:16 +0800

    Initialize score to zero.

--- game_assets/scoreboard.py
+++ game_assets/scoreboard.py
@@ -2,4 +2,6 @@ from game_assets.base import GameElement
 import winsound
 
 class Scoreboard(GameElement):
-    
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.score = 0
