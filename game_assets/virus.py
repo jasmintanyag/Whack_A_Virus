f3c1f16c899b16d4cc9afc25a06d23bc140f410d
jasmintanyag <tanyagjasmin@gmail.com>
2025-06-24 15:05:04 +0800

    Initialize.

--- game_assets/virus.py
+++ game_assets/virus.py
@@ -2,3 +2,7 @@ from game_assets.base import GameElement
 import random
 
 class Virus(GameElement):
+    def __init__(self, screen, image_path):
+        super().__init__(screen)
+        self.screen.addshape(image_path)
+        self.turtle.shape(image_path)
